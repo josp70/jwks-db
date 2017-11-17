6e10cd28cac04129e798ee6e8d4a0124d469faa8
Jorge Pérez <josp.jorge@gmail.com>
2017-11-17 14:11:23 +0100

    fixes #2, linting

--- examples/sample_01.js
+++ examples/sample_01.js
@@ -1,15 +1,17 @@
-const jwks = require('jwks-db');
+const jwks = require('../index.js');
 const {MongoClient} = require('mongodb');
 
-MongoClient.connect('mongodb://localhost:27017/msvc_user').then(db=>{
+MongoClient.connect('mongodb://localhost:27017/test_jwks')
+  .then((db) => {
     console.log('db connected');
-    jwks.connect(db).then(ks => {
-        console.log(ks.toJSON());
-        const all = ks.all({ kty: 'RSA' });
-        
-        console.log(all[0]);
-        db.close().then(_=>{
-            console.log('db closed');
+    jwks.connect(db).then((ks) => {
+      console.log(ks.toJSON());
+      const all = ks.all({kty: 'RSA'});
+
+      console.log(all[0]);
+      db.close()
+        .then(() => {
+          console.log('db closed');
         });
     });
-});
+  });
